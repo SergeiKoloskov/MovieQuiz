75007e3cc3232eab8f6c58bcf01fa27128a1ccff
Sergei Koloskov <ser.koloskov13@gmail.com>
2024-04-27 14:31:31 +0300

    Sprint 7 Homework polished - minor problems fixed

--- MovieQuizTests/MovieQuizTests.swift
+++ MovieQuizTests/MovieQuizTests.swift
@@ -24,14 +24,11 @@ class MovieQuizTests: XCTestCase {
     func testAddition() throws {
         // Given
         let arithmeticOperations = ArithmeticOperations()
-        let num1 = 1
-        let num2 = 2
-       
-        
+
         let expectation = expectation(description: "Addition function expectation")
        
         // When
-        arithmeticOperations.addition(num1: num1, num2: num2) { result in
+        arithmeticOperations.addition(num1: 1, num2: 2) { result in
             // Then
             XCTAssertEqual(result, 3)
             expectation.fulfill()
